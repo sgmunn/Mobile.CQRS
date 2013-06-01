8c926067aab6167e95195031d958a9ae2c871d46
Greg Munn <dev-admin@nxsoftwaretech.com>
2013-06-01 21:40:35 +1000

    Clean up of classes to separate files.

--- Core/Domain/ReadModelBuilderAgent.cs
+++ Core/Domain/ReadModelBuilderAgent.cs
@@ -1,54 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-
-
-using System.Reflection;
-using System.Collections.Concurrent;
-using Mobile.CQRS.SQLite.Domain;
-using Mobile.CQRS.SQLite;
-using Mobile.CQRS.Serialization;
-using System.Threading;
-using System.Threading.Tasks;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReadModelBuilderAgent.cs" company="sgmunn">
+//   (c) sgmunn 2012  
+//
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//   documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+//   the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+//   to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+//   the Software.
+// 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+//   THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+//   CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//   IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Mobile.CQRS.Domain
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Mobile.CQRS.Reactive;
-
-    public interface IReadModelBuilderFaulted : IEvent
-    {
-        Exception Fault { get; }
-    }
-    
-    public sealed class ReadModelBuilderFaultedEvent : IReadModelBuilderFaulted
-    {
-        public ReadModelBuilderFaultedEvent(Guid identity, Exception ex)
-        {
-            this.Identity = identity;
-            this.Fault = ex;
-        }
-
-        public Guid Identity { get; set; }
-        public Exception Fault { get; set; }
-    }
-
-    public interface IReadModelBuilderAgent 
-    {
-        bool IsStarted { get; }
-
-        bool IsFaulted { get; }
-
-        Exception Fault { get; }
-
-        void Start();
-
-        void Stop();
-         
-        void Trigger();
-    }
 
     public sealed class ReadModelBuilderAgent : IReadModelBuilderAgent
     {
@@ -125,7 +102,7 @@ namespace Mobile.CQRS.Domain
                 return;
             }
 
-            var process = Task.Factory.StartNew(() => {
+            Task.Factory.StartNew(() => {
                 if (Interlocked.Exchange(ref this.locker, 1) == 0)
                 {
                     try
