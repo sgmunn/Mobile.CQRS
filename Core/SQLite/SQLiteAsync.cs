8919812c0113516dcf1a61fc3668d003ce7d04be
Greg Munn <dev-admin@nxsoftwaretech.com>
2013-06-02 22:23:01 +1000

    Changed namespace of SQLiteAsync to fit with Mobile.CQRS

--- Core/SQLite/SQLiteAsync.cs
+++ Core/SQLite/SQLiteAsync.cs
@@ -28,7 +28,7 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SQLite
+namespace Mobile.CQRS.SQLite
 {
 	public partial class SQLiteAsyncConnection
 	{
